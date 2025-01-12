0677064e246d916181c6a6b55e87ebcaadfb3c13
mahemud <mahemudborgave@gmail.com>
2025-01-12 11:06:03 +0530

    tesla clone project added

---
+++ Car-React/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import "../src/style.css";
+
+function Navbar() {
+    return(
+        <>
+            <nav>
+                <p>MDCars</p>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Variety</li>
+                    <li>Contact</li>
+                </ul>
+                <p>Log In</p>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
---
+++ Car-React/src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Components/Navbar.jsx";
+import Showcase from "../Components/Showcase.jsx";
+
+function App() {
+  return(
+    <>
+      <Showcase />  
+    </>
+  )
+}
+
+export default App;
